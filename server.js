941dcae9b7cb38d272798c598145ffc511fa03ed
Tirawat Pompakunsawat <bamiowen@gmail.com>
2025-03-25 17:59:55 +1000

    edit server.js

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ const webServer = express();
 // middle ware
 webServer.use(express.json());
 webServer.use(express.urlencoded({ extended: true }));
+
+webServer.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 webServer.use(
   cors({
     origin: process.env.CORS_ORIGIN,
